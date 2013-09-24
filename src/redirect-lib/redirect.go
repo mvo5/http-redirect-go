d8d0ea154d9ec32b5df0cd9055a47521a53de9e7
Michael Vogt <mvo@debian.org>
2013-09-24 13:14:07 +0200

    add TODO and cleanup FIXMEs

--- src/redirect-lib/redirect.go
+++ src/redirect-lib/redirect.go
@@ -4,20 +4,16 @@ import (
     "net/http"
 )
 
-// FIXME:
-// - route /*
-
 // FIXME: is there a more elegant way than a "package" wide var?
 var global_redirect_to string;
 
 func redirectHandler(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, global_redirect_to + r.URL.String(), 
 		      http.StatusMovedPermanently)
-
 }
 
 func DoRedirect(redirect_from, redirect_to string) {
 	global_redirect_to = redirect_to
-    http.HandleFunc("/", redirectHandler)
-    http.ListenAndServe(redirect_from, nil)
+        http.HandleFunc("/", redirectHandler)
+        http.ListenAndServe(redirect_from, nil)
 }
